9653b89c6203cc179b238952e67a59a7ac68e966
JANGJI0 <qlvm2002@gmail.com>
2025-05-27 17:03:18 +0900

    [장지영] 사인중복제거 쿼리수정

--- src/main/java/com/example/signapp/mapper/SignMapper.java
+++ src/main/java/com/example/signapp/mapper/SignMapper.java
@@ -1,6 +1,8 @@
 package com.example.signapp.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
 import com.example.signapp.dto.SignForm;
 
 @Mapper
@@ -8,4 +10,5 @@ public interface SignMapper {
     int addSignLevel1(SignForm signForm);  // level1 결재 처리
     int addSignLevel2(SignForm signForm);  // level2 결재 처리
     SignForm selectSignByDocumentNo(int documentNo); // 서명 불러오기
+    int deleteOldSignsExceptLatest(@Param("documentNo") int documentNo); // 서명 중복된것 삭제
 }
